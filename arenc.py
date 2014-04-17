aa76646a19dcfbf07639cf9b531cf0199829d90d
Constantin Berzan <cberzan@gmail.com>
2014-04-16 18:19:22 -0700

    more tests + bugfixes

--- arenc.py
+++ arenc.py
@@ -1,3 +1,34 @@
+# This is a simple arithmetic encoder / decoder as described on Wikipedia.
+# I think it's the same as Rubin's encoder described in EIDMA section 6.1.1.
+# It is not as fancy as what the CTW implementation uses. See EIDMA section
+# 6.1.3 for limitations of this encoder. In particular, in split_interval,
+# total_length can be small, leading to imprecise splitting. For example, if
+# total_length is 2, the interval is split in half, regardless of p0.
+
+
+def split_interval(a, b, p0):
+    """
+    Split [a, b] into [a0, b0] and [a1, b1] according to p0.
+    """
+    assert 0 <= a <= 0xFF
+    assert 0 <= b <= 0xFF
+    total_length = b - a + 1
+    assert total_length >= 2
+    new_length_0 = int(p0 * total_length)
+
+    # Make sure we don't produce intervals of zero length:
+    if new_length_0 == 0:
+        new_length_0 = 1
+    elif new_length_0 == total_length:
+        new_length_0 = total_length - 1
+
+    a0, b0 = a, a + new_length_0 - 1
+    a1, b1 = a + new_length_0, b
+    assert a0 <= b0
+    assert a1 <= b1
+    return a0, b0, a1, b1
+
+
 class ArithmeticEncoder(object):
     """
     Arithmetic encoder.
@@ -49,15 +80,14 @@ class ArithmeticEncoder(object):
         # Subdivide current interval according to p0, and take one of the two
         # resulting sub-intervals, based on bit.
         old_a, old_b = self.cur_ival_a, self.cur_ival_b
-        new_length_0 = int(p0 * (self.cur_ival_b - self.cur_ival_a + 1))
+        a0, b0, a1, b1 = split_interval(self.cur_ival_a, self.cur_ival_b, p0)
         if bit == 0:
-            self.cur_ival_b = self.cur_ival_a + new_length_0 - 1
+            self.cur_ival_a, self.cur_ival_b = a0, b0
         else:
-            self.cur_ival_a = self.cur_ival_a + new_length_0
+            self.cur_ival_a, self.cur_ival_b = a1, b1
         print (
             "p0={} bit={} old_a={:08b} old_b={:08b} new_a={:08b} new_b={:08b}"
             .format(p0, bit, old_a, old_b, self.cur_ival_a, self.cur_ival_b))
-        assert self.cur_ival_a < self.cur_ival_b
 
         self.msg_len += 1
 
@@ -68,8 +98,9 @@ class ArithmeticEncoder(object):
             for pos in range(7, -1, -1):
                 self._emit_bit(1 if self.cur_ival_a & (1 << pos) else 0)
 
-            # If we have an incomplete byte, pad with zeros.
+            # Pad with zeros, at least once, and up to a complete byte.
             print "padding"
+            self._emit_bit(0)
             while self.bits_in_cur_byte:
                 self._emit_bit(0)
 
@@ -119,7 +150,7 @@ class ArithmeticDecoder(object):
         assert not self.decoded_buffer, "finalized; can't decode more bits"
         assert 0 <= p0 <= 1
 
-        if 8 * self.byte_index + self.bit_index >= self.msg_len:
+        if 8 * len(self.decoded_bytes) + self.bits_in_cur_byte >= self.msg_len:
             raise IndexError("ran out of bits to decode")
 
         # Scale current interval, and advance index into encoded_buffer.
@@ -150,20 +181,19 @@ class ArithmeticDecoder(object):
         # Subdivide current interval according to p0, and take one of the two
         # resulting sub-intervals, depending on where encoded_byte falls.
         old_a, old_b = self.cur_ival_a, self.cur_ival_b
-        new_length_0 = int(p0 * (self.cur_ival_b - self.cur_ival_a + 1))
-        if encoded_byte <= self.cur_ival_a + new_length_0 - 1:
+        a0, b0, a1, b1 = split_interval(self.cur_ival_a, self.cur_ival_b, p0)
+        if encoded_byte <= b0:
             decoded_bit = 0
-            self.cur_ival_b = self.cur_ival_a + new_length_0 - 1
+            self.cur_ival_a, self.cur_ival_b = a0, b0
         else:
             decoded_bit = 1
-            self.cur_ival_a = self.cur_ival_a + new_length_0
+            self.cur_ival_a, self.cur_ival_b = a1, b1
         print (
             "p0={} byte_i={} bit_i={} encoded_byte={:08b} "
             "old_a={:08b} old_b={:08b} new_a={:08b} new_b={:08b}"
             .format(
                 p0, self.byte_index, self.bit_index, encoded_byte,
                 old_a, old_b, self.cur_ival_a, self.cur_ival_b))
-        assert self.cur_ival_a < self.cur_ival_b
 
         self._emit_bit(decoded_bit)
         return decoded_bit
--- test_arenc.py
+++ test_arenc.py
@@ -1,7 +1,9 @@
 import nose
+import random
 
 from arenc import ArithmeticDecoder
 from arenc import ArithmeticEncoder
+from arenc import split_interval
 from util import byte_bit
 from util import pretty_bin_buffer
 
@@ -17,13 +19,25 @@ def test_byte_bit():
     nose.tools.assert_equal(byte_bit(0x41, 7), 1)
 
 
-def test_const_symmetric_prob():
-    bits = [0, 1, 0, 0, 1]
+def test_split_interval():
+    nose.tools.assert_equal(
+        split_interval(0x00, 0xFF, 0.5),
+        (0x00, 0x7F, 0x80, 0xFF))
+    nose.tools.assert_equal(
+        split_interval(0, 0xFF, 0.0001),
+        (0x00, 0x00, 0x01, 0xFF))
+    nose.tools.assert_equal(
+        split_interval(0, 0xFF, 0.9999),
+        (0x00, 0xFE, 0xFF, 0xFF))
+
+
+def _encode_decode(bits, probs):
+    assert len(bits) == len(probs)
 
     print "---------------- encoding"
     encoder = ArithmeticEncoder()
-    for bit in bits:
-        encoder.encode_bit(0.5, bit)
+    for bit, prob in zip(bits, probs):
+        encoder.encode_bit(prob, bit)
     msg_len, enc_buf = encoder.get_encoded_data()
     print "enc_buf:", pretty_bin_buffer(enc_buf)
     nose.tools.assert_equal(msg_len, len(bits))
@@ -32,7 +46,7 @@ def test_const_symmetric_prob():
     decoder = ArithmeticDecoder(msg_len, enc_buf)
     bits_back = []
     for i in xrange(msg_len):
-        bit = decoder.decode_bit(0.5)
+        bit = decoder.decode_bit(probs[i])
         bits_back.append(bit)
     msg_len_back, dec_buf = decoder.get_decoded_data()
     print "dec_buf:", pretty_bin_buffer(dec_buf)
@@ -51,4 +65,40 @@ def test_const_symmetric_prob():
     padding_bits = []
     for i in xrange(bit_index + 1, 8):
         padding_bits.append(byte_bit(ord(dec_buf[byte_index]), i))
-    nose.tools.assert_equal(padding_bits, [0] * (8 - msg_len % 8))
+    nose.tools.assert_equal(padding_bits, [0] * ((8 - msg_len % 8) % 8))
+
+
+def test_short_msg_const_symmetric_prob():
+    bits = [0, 1, 0, 0, 1]
+    probs = [0.5] * len(bits)
+    _encode_decode(bits, probs)
+
+
+def test_short_msg_const_asymmetric_prob():
+    for prob in [0.1, 0.2, 0.3, 0.7, 0.8, 0.9]:
+        print "************* trying prob={}".format(prob)
+        bits = [0, 1, 0, 0, 1]
+        probs = [prob] * len(bits)
+        _encode_decode(bits, probs)
+
+
+def test_short_msg_changing_prob():
+    bits = [0, 1, 0, 0, 1, 1]
+    probs = [0.1, 0.2, 0.3, 0.7, 0.8, 0.9]
+    _encode_decode(bits, probs)
+
+
+def random_test_long_msg_changing_prob():
+    msg_len = 1000
+    bits = []
+    probs = []
+    random.seed(666)
+    for i in xrange(msg_len):
+        bits.append(random.choice((0, 1)))
+        prob = random.random()
+        if prob < 0.01:
+            prob = 0.01
+        if prob > 0.99:
+            prob = 0.99
+        probs.append(prob)
+    _encode_decode(bits, probs)
