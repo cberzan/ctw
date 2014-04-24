b5e6ff6ddb8d5680790927dd83a660138342bffb
Constantin Berzan <cberzan@gmail.com>
2014-04-24 13:03:55 -0700

    ctw encode/decode pipeline, and tests
    (test_large currently fails because probs go down to zero -- need log probs)

---
+++ ctw.py
@@ -0,0 +1,35 @@
+from arenc import ArithmeticDecoder
+from arenc import ArithmeticEncoder
+from util import byte_bit
+from wct_binary import WCTBinary
+
+
+def encode(data, max_depth=3):
+    """
+    Encode data using WCTBinary and return (msg_len, enc_data).
+    """
+    tree = WCTBinary(max_depth)
+    encoder = ArithmeticEncoder()
+    context = [0] * max_depth
+    for byte in data:
+        for i in xrange(8):
+            bit = byte_bit(ord(byte), i)
+            encoder.encode_bit(tree.get_p0(context), bit)
+            tree.update(context, bit)
+            context = context[1:] + [bit]
+    print "tree size is", tree.next_id
+    return encoder.get_encoded_data()
+
+
+def decode(msg_len, enc_data, max_depth=3):
+    """
+    Decode enc_data using WCTBinary and return (msg_len, dec_data).
+    """
+    tree = WCTBinary(max_depth)
+    decoder = ArithmeticDecoder(msg_len, enc_data)
+    context = [0] * max_depth
+    for i in xrange(msg_len):
+        bit = decoder.decode_bit(tree.get_p0(context))
+        tree.update(context, bit)
+        context = context[1:] + [bit]
+    return decoder.get_decoded_data()
